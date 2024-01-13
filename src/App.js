d7e770a26613b81ab4859fa86f3af2c4ce4d7123
Jonathan Kong <275568635@qq.com>
2024-01-13 19:38:13 +0800

    add

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // import Main from "./components/router/version5/demo5/main"
-import Demo from "./components/react/demo28"
+import Demo from "./components/react/demo8"
 function App() {
   return (
     <div>
